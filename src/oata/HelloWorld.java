b8ec60903ea74346e7ea8d44a42deadf23d51a4e
Anuj Sharma <anuj_sharma401@yahoo.com>
2020-01-19 20:29:18 +0530

    Update HelloWorld.java

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,6 +2,6 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World Jenkins !!");
+        System.out.println("Hello World Web Hook !!");
     }
 }
